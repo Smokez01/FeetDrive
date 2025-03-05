6628a22924de0e4c5078b51b7d80e433112691aa
xNeto7 <mateja2006@icloud.com>
2025-03-05 11:24:44 +0100

    seffxcg

--- Projekt.py
+++ Projekt.py
@@ -16,16 +16,28 @@ def add_file():
     if file_path:
         Data.append(file_path)
 
+def remove_from_cloud():
+    selected_file_index = selected_file.curseselection()
+    if selected_file_index:
+        data.pop(selected_file_index[0])
+        update_data()
+
 upload = Image.open("upload.png")
 uploadPhoto = ImageTk.PhotoImage(upload)
 
 download = Image.open("download.png")
 downloadPhoto = ImageTk.PhotoImage(download)
 
+delete = Image.open("delete.png")
+deletePhoto = ImageTk.PhotoImage(delete)
+
 upload_Button=Button(f, image=uploadPhoto, command=add_file, bg="dark violet", relief=FLAT,activebackground="dark violet")
-upload_Button.place(x=10, y=10)
+upload_Button.place(x=5, y=10)
 
 download_Button = Button(f, image=downloadPhoto, bg="dark violet", relief=FLAT,activebackground="dark violet")
-download_Button.place(x=180, y=10)
+download_Button.place(x=90, y=10)
+
+delete_Button=Button(f, image=deletePhoto, command=remove_from_cloud, bg="dark violet", relief=FLAT,activebackground="dark violet")
+delete_Button.place(x=175, y=10)
 
 f.mainloop()
