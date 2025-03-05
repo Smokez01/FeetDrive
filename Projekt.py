ce412d36d0264b89ea2a1b2bed988a6bd554df11
xNeto7 <mateja2006@icloud.com>
2025-03-05 09:39:35 +0100

    hi

--- Projekt.py
+++ Projekt.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image, ImageTk
 
 f=Tk()
 f.title("Füße")
@@ -7,5 +8,9 @@ f.geometry("800x600")
 f. resizable(width=False, height=False)
 f.configure(bg="dark violet")
 
+folder = Image.open("folder.png")
+folderPhoto = ImageTk.PhotoImage(folder)
 
+folder_Button=Button(f, image=folderPhoto, bg="dark violet", relief=FLAT)
+folder_Button.place(x=400, y=300)
 f.mainloop()
