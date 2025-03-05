c6e1f1abf84e6c57a3d3127ad83ac80a0e1fee81
xNeto7 <mateja2006@icloud.com>
2025-03-05 11:11:38 +0100

    hi

--- Projekt.py
+++ Projekt.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+Data = []
+
 f=Tk()
 f.title("FeetDrive")
 f.geometry("250x100")
@@ -9,13 +11,18 @@ f. resizable(width=False, height=False)
 f.configure(bg="dark violet")
 f.iconbitmap("Feet.ico")
 
+def add_file():
+    file_path = filedialog.askopenfile(title="Datei auswählen")
+    if file_path:
+        Data.append(file_path)
+
 upload = Image.open("upload.png")
 uploadPhoto = ImageTk.PhotoImage(upload)
 
 download = Image.open("download.png")
 downloadPhoto = ImageTk.PhotoImage(download)
 
-upload_Button=Button(f, image=uploadPhoto, bg="dark violet", relief=FLAT,activebackground="dark violet")
+upload_Button=Button(f, image=uploadPhoto, command=add_file, bg="dark violet", relief=FLAT,activebackground="dark violet")
 upload_Button.place(x=10, y=10)
 
 download_Button = Button(f, image=downloadPhoto, bg="dark violet", relief=FLAT,activebackground="dark violet")
