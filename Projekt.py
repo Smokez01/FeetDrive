6f718b5aefbffd00a90b13485c674918f0b38433
Jiajie8 <yingjiajie2007@gmail.com>
2025-03-05 10:28:55 +0100

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -3,10 +3,11 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 
 f=Tk()
-f.title("Füße")
-f.geometry("200x400")
+f.title("FeetDrive")
+f.geometry("250x400")
 f. resizable(width=False, height=False)
 f.configure(bg="dark violet")
+f.iconbitmap("Feet.ico")
 
 upload = Image.open("upload.png")
 uploadPhoto = ImageTk.PhotoImage(upload)
@@ -18,6 +19,8 @@ upload_Button=Button(f, image=uploadPhoto, bg="dark violet", relief=FLAT,activeb
 upload_Button.place(x=10, y=300)
 
 download_Button = Button(f, image=downloadPhoto, bg="dark violet", relief=FLAT,activebackground="dark violet")
-download_Button.place(x=150, y=300)
+download_Button.place(x=180, y=300)
+
+
 
 f.mainloop()
