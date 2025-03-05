1432ef15902fe5c81113ca5e6002d1967ef44361
xNeto7 <mateja2006@icloud.com>
2025-03-05 09:27:36 +0100

    hi

---
+++ Projekt.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import filedialog
+
+f=Tk()
+f.title("Füße")
+f.geometry("800x600")
+f. resizable(width=False, height=False)
+f.configure(bg="black")
+
+f.mainloop()
