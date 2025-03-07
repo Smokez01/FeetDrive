a528485d26b177fbaf6846d66eafaefaefc4c947
xNeto7 <mateja2006@icloud.com>
2025-03-07 11:15:48 +0100

    hi

--- Projekt.py
+++ Projekt.py
@@ -8,7 +8,7 @@ Data = []
 
 f=Tk()
 f.title("FeetDrive")
-f.geometry("250x100")
+f.geometry("250x180")
 f. resizable(width=False, height=False)
 f.configure(bg="dark violet")
 f.iconbitmap("images/Feet.ico")
@@ -39,19 +39,19 @@ downloadPhoto = ImageTk.PhotoImage(download)
 delete = Image.open("images/delete.png")
 deletePhoto = ImageTk.PhotoImage(delete)
 
-Open = Image.open("image/open.png")
+Open = Image.open("images/open.png")
 OpenPhoto = ImageTk.PhotoImage(Open)
 
 upload_Button=Button(f, image=uploadPhoto, command=add_file, bg="dark violet", relief=FLAT,activebackground="dark violet")
-upload_Button.place(x=5, y=50)
+upload_Button.place(x=5, y=100)
 
 download_Button = Button(f, image=downloadPhoto, bg="dark violet", relief=FLAT,activebackground="dark violet")
-download_Button.place(x=90, y=50)
+download_Button.place(x=90, y=100)
 
 delete_Button=Button(f, image=deletePhoto, command=remove_from_cloud, bg="dark violet", relief=FLAT,activebackground="dark violet")
-delete_Button.place(x=175, y=50)
+delete_Button.place(x=175, y=100)
 
-Open_Button=Button(f, image= OpenPhoto, command=open_html, bg= "dark violet", relief=Flat,activebackground="dark violet")
+Open_Button=Button(f, image= OpenPhoto, command=open_html, bg= "dark violet", relief=FLAT,activebackground="dark violet")
 Open_Button.place(x=90, y=10)
 
 f.mainloop()
